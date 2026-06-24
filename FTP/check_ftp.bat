8232835970c19b6c67bb0eeeacddd2e2698f1aed
Huy Võ <hvo345446@gmail.com>
2026-06-24 16:17:04 +0700

    FTP

---
+++ FTP/check_ftp.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==========================================
+echo KIEM TRA KET NOI FTP SERVER
+echo ==========================================
+echo.
+ftp 192.168.229.40
+echo.
+pause
