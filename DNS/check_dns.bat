68c1b2c7dd1c599330e2eb44451c13875202d569
Huy Võ <hvo345446@gmail.com>
2026-06-24 16:16:10 +0700

    DNS

---
+++ DNS/check_dns.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ==========================================
+echo KIEM TRA PHAN GIAI TEN MIEN DNS SERVER
+echo ==========================================
+echo.
+echo [1] Kiem tra phan giai thuan (Ten mien -> IP):
+nslookup test.vobahuy.com
+echo.
+echo [2] Kiem tra phan giai nghich (IP -> Ten mien):
+nslookup 192.168.229.40
+echo.
+pause
